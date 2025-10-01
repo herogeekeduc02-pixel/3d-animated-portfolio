13b0db4298e400282861f841b0ecbf9266f7fb67
herogeekeduc02-pixel <herogeekeduc02@gmail.com>
2025-10-01 16:24:31 +0000

    Códigos dia 01/10

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             </h1>
             <div className="awards">
                 <h2>Dev Experiente !</h2>
-                <p>Experiencias</p>
+                <p>Programador com ampla gama de conhecimentos.</p>
                 <div className="awardList">
                     <img src="/award1.png" alt="" />
                     <img src="/award2.png" alt="" />
@@ -18,7 +18,7 @@ const Hero = () => {
                 </div>
             </div>
             {/* Scroll SVG*/}
-            <a href="services">
+            <a href="#services">
                 <svg
                     width="50px"
                     height="50px"
@@ -27,12 +27,23 @@ const Hero = () => {
                     xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
-                        
+                        d="M5 9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9Z"
+                        stroke="white"
+                        strokeWidth="1"
+                    />
 
+                    <path
+                        d = "M12 5V8"
+                        stroke="white"
+                        strokeWidth="1"
+                        strokeLinecap="round" 
+                    />
                 </svg>
             </a>
         </div>
-        <div className="hSection right"></div>
+        <div className="hSection right">
+            
+        </div>
 
     </div>;
 };

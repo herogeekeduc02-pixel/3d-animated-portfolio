6b16535fafed9c6789c67cb35bc203e07ec87574
herogeekeduc02-pixel <herogeekeduc02@gmail.com>
2025-10-22 16:04:33 +0000

    códogos dia 22/10

--- src/components/services/computador/ComputadorContainer.jsx
+++ src/components/services/computador/ComputadorContainer.jsx
@@ -1,17 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Computador } from "./Computador";
-import { OrbitControls, Stage } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const ComputadorContainer = () => {
    return (
       <Canvas style={{ width: '100%', height: '400px' }} camera={{ position: [0, 0, 5], fov: 50 }}>
             <Suspense fallback={"loading..."}>
-               <Stage>
-                  
-               </Stage>
-               <Computador />
-               <OrbitControls enableZoom={false} />
+               <Stage environment="night" intensity={0.5}>
+                  <Computador/>
+               </Stage> 
+               <OrbitControls enableZoom={false} autoRotate/>
+               <PerspectiveCamera  position={[-1, 0, 1.8]} zoom={0.8} makeDefault/>
             </Suspense>
       </Canvas>
    )

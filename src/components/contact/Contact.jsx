fafc4d8557a4a4d1e252d9602d6455545ec35696
herogeekeduc02-pixel <herogeekeduc02@gmail.com>
2025-11-13 16:15:13 +0000

    Códigos do dia 13/11

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,28 +3,35 @@ import emailjs from "@emailjs/browser";
 import {useRef, useState} from "react";
 
 import "./contact.css";
+import { span } from "motion/react-client";
 
-
+import { motion } from "motion/react"
 
 const Contact = () => {
    const [success, setSuccess] = useState(false);
    const [error, setError] = useState(false);
+
+   const ref = useRef();
+    
    const sendEmail = (e) =>{
       e.preventDefault();
       emailjs
          .sendForm(
             import.meta.env.VITE_SERVICE_ID,
             import.meta.env.VITE_TEMPLATE_ID, 
-            form.current, {
+            e.target, {
                publicKey: import.meta.env.VITE_PUBLIC_KEY,
             }
          )
          .them(
             () => {
-               console.log('SUCCESS!');
+               setSucces(true);
+               setError(false);
             },
             (erro) => {
-               console.log('FAILED...', erro.text);
+               console.log(error);
+               setError(true);
+               setSuccess(false);
             }
          );
    };
@@ -35,17 +42,19 @@ const Contact = () => {
                <h1 className="cTitle">Vamos manter em contato</h1> 
                <div className="formItem">
                   <label>Nome</label>
-                  <input type="text" placeholder="Jonh Silva" />
+                  <input type="text" name = "user_username" placeholder="Jonh Silva" />
                </div>
                <div className="formItem">
                   <label>Email</label>
-                  <input type="email" placeholder="Jonh@gamil.com" />
+                  <input type="email" name = "user_Email" placeholder="Jonh@gamil.com" />
                </div>
                <div className="formItem">
                   <label>Mensagem</label>
-                  <textarea rows={10} placeholder="Digite sua mensagem"></textarea>
+                  <textarea rows={10} name = "user_message" placeholder = "Digite sua mensagem"></textarea>
                </div>
                <button className="formButton">Enviar</button>
+               {success && <span>Sua mensagem foi enviada com sucessp</span>}
+               {error && <span>Algo deu errado !</span>} 
             </form>
          </div>
          <div className="cSection">SVG</div>
---
+++ src/components/contact/ContactSVG.jsx
@@ -0,0 +1,10 @@
+import { motion } from "motion/react"
+
+
+const ContactSvg = () => {
+    return(
+        <div className="">ContactSVG</div>
+    )
+}
+
+export default ContactSvg
